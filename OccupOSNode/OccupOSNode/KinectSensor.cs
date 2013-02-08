18515ee8bfc36d961917d7825fc60a7ccb5b3ddc
Richy Davis <rdavis4200@gmail.com>
2013-02-08 16:52:35 +0000

    Added Kinect SDK files to libs

--- OccupOSNode/OccupOSNode/KinectSensor.cs
+++ OccupOSNode/OccupOSNode/KinectSensor.cs
@@ -1,7 +1,13 @@
 using System;
 using Microsoft.SPOT;
+using Microsoft.Kinect;
 
 namespace OccupOSNode {
     class KinectSensor : Sensor {
+        KinectSensor ksensor;
+
+        public KinectSensor(String id) : base(id) {
+
+        }
     }
 }
