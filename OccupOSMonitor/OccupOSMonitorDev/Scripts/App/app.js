a6448b2057fbb536bf9c02203c42a6945e9dfea3
Globegitter <markus.padourek@gmail.com>
2013-02-13 18:51:53 +0000

    Deleted files manually

--- OccupOSMonitor/OccupOSMonitorDev/Scripts/App/app.js
+++
@@ -1,9 +0,0 @@
-﻿/// <reference path="../vendor/Ember/ember.js" />
-
-window.App = Em.Application.create({
-    VERSION: '0.1.0',
-    name: 'OccupOS Monitor',
-    author: 'Markus Padourek'
-});
-
-
