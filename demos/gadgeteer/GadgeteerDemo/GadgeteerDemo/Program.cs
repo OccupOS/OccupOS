e6ae486af61cf2435e4e854be54d4965b096ce49
Morrison Cole <admin@morrisoncole.co.uk>
2013-02-23 13:35:25 +0000

    Added WiFi network test

--- demos/gadgeteer/GadgeteerDemo/GadgeteerDemo/Program.cs
+++ demos/gadgeteer/GadgeteerDemo/GadgeteerDemo/Program.cs
@@ -11,6 +11,18 @@ namespace GadgeteerDemo
         // This method is run when the mainboard is powered up or reset.   
         void ProgramStarted()
         {
+            Debug.Print("Scanning for WiFi networks");
+            GHI.Premium.Net.WiFiNetworkInfo[] wiFiNetworkInfo = wifi_RS21.Interface.Scan();
+            if (wiFiNetworkInfo != null)
+            {
+                Debug.Print("Found WiFi network");
+                Debug.Print("0: " + wiFiNetworkInfo[0].SSID);
+            }
+            else
+            {
+                Debug.Print("Didn't find any WiFi networks");
+            }
+
             timer.Tick += new GT.Timer.TickEventHandler(timer_Tick);
             timer.Start();
 
